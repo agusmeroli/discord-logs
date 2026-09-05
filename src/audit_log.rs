4abf170813ec8eb5ef0251addd9b489bbd53125f
Alberto Gusmeroli <96133902+guss-alberto@users.noreply.github.com>
2026-09-05 17:11:32 +0200

    cleanup audit log

--- src/audit_log.rs
+++ src/audit_log.rs
@@ -1,7 +1,7 @@
 use serenity::{
     all::{
-        AuditLogEntry, Context, GenericChannelId, GuildId, MemberAction, MessageAction, UserId,
-        audit_log::Action,
+        AuditLogEntry, AuditLogEntryId, Context, GenericChannelId, GuildId, MemberAction,
+        MessageAction, UserId, audit_log::Action,
     },
     nonmax::NonMaxU8,
 };
@@ -32,18 +32,14 @@ pub async fn init_audit_log(guild_id: GuildId, ctx: &Context, pool: &PgPool) {
             1
         };
 
-        let result = sqlx::query("SELECT update_audit_count($1, $2)")
+        if let Err(e) = sqlx::query("SELECT update_audit_count($1, $2)")
             .bind(entry.id.get() as i64)
             .bind(count)
-            .fetch_one(pool)
-            .await;
-
-        match result {
-            Ok(row) => row.get::<Option<i32>, _>(0),
-            Err(e) => {
-                log::error!("Failed to upsert audit log: {}", e);
-                continue;
-            }
+            .execute(pool)
+            .await
+        {
+            log::error!("Failed to upsert audit log: {}", e);
+            continue;
         };
     }
 }
@@ -97,26 +93,9 @@ pub async fn get_message_deleted_entry(
             1
         };
 
-        let result = sqlx::query("SELECT update_audit_count($1, $2)")
-            .bind(entry.id.get() as i64)
-            .bind(count)
-            .fetch_one(pool)
-            .await;
-
-        let old_count = match result {
-            Ok(row) => row.get::<Option<i32>, _>(0),
-            Err(e) => {
-                log::error!("Failed to upsert audit log: {}", e);
-                continue;
-            }
-        };
-
-        // if the entry was old and didn't change
-        if old_count.is_none() {
-            continue;
+        if is_new_entry(&entry.id, count, pool).await {
+            return Some(entry);
         }
-
-        return Some(entry);
     }
 
     return None;
@@ -154,25 +133,77 @@ pub async fn get_ban_or_kick_event(
             _ => continue,
         }
 
-        let result = sqlx::query("SELECT update_audit_count($1, 0)")
-            .bind(entry.id.get() as i64)
-            .fetch_one(pool)
-            .await;
+        if is_new_entry(&entry.id, 1, pool).await {
+            return Some(entry);
+        }
+    }
+    return None;
+}
 
-        let old_count = match result {
-            Ok(row) => row.get::<Option<i32>, _>(0),
-            Err(e) => {
-                log::error!("Failed to upsert audit log: {}", e);
-                continue;
-            }
-        };
+pub async fn get_bulk_delete_entry(
+    guild_id: &GuildId,
+    channel_id: &GenericChannelId,
+    msg_count: usize,
+    ctx: &Context,
+    pool: &PgPool,
+) -> Option<AuditLogEntry> {
+    let logs = guild_id
+        .audit_logs(
+            &ctx.http,
+            Some(Action::Message(MessageAction::BulkDelete)),
+            None,
+            None,
+            None,
+            NUMBER_OF_LOG_LIMIT,
+        )
+        .await;
 
-        // if the entry was old and didn't change
-        if old_count.is_none() {
+    let logs = match logs {
+        Ok(logs) => logs,
+        Err(e) => {
+            log::error!("Failed to fetch audit logs for bulk delete: {}", e);
+            return None;
+        }
+    };
+
+    for entry in logs.entries {
+        if let Some(options) = &entry.options
+            && let Some(msg_channel_id) = options.channel_id
+            && msg_channel_id.get() != channel_id.get()
+        {
             continue;
         }
 
-        return Some(entry);
+        if let Some(options) = &entry.options
+            && let Some(count) = options.count
+            && msg_count == count.get() as usize
+        {
+        } else {
+            continue;
+        };
+
+        if is_new_entry(&entry.id, msg_count as i32, pool).await {
+            return Some(entry);
+        }
     }
+
     return None;
+}
+
+async fn is_new_entry(id: &AuditLogEntryId, count: i32, pool: &PgPool) -> bool {
+    let result: Result<Option<i32>, sqlx::Error> =
+        sqlx::query_scalar("SELECT update_audit_count($1, $2)")
+            .bind(id.get() as i64)
+            .bind(count)
+            .fetch_one(pool)
+            .await;
+
+    match result {
+        Ok(Some(_)) => true,
+        Err(e) => {
+            log::error!("Failed to upsert audit log: {}", e);
+            false
+        }
+        _ => false,
+    }
 }
